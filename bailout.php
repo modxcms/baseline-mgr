ea8487f68c45d215868bc4203b1dd03459a5a0b6
Wayne Roddy <wayneroddy@Waynes-MacBook-Pro.local>
2014-07-16 10:05:07 -0400

    Bailout

--- bailout.php
+++ bailout.php
@@ -2,18 +2,37 @@
 //run this file if you mess up your new manager theme and get locked out.
 //delete after done
 
-$mysqli = new mysqli("localhost", "cXXX", "XXX", "instance_cXXX_modx");
+@include(dirname(__FILE__) . '/config.core.php');
+if (!defined('MODX_CORE_PATH')) define('MODX_CORE_PATH', dirname(__FILE__) . '/core/');
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
+if (!@include_once (MODX_CORE_PATH . "model/modx/modx.class.php")) {
+    $errorMessage = 'Site temporarily unavailable';
+    @include(MODX_CORE_PATH . 'error/unavailable.include.php');
+    header('HTTP/1.1 503 Service Unavailable');
+    echo "<html><title>Error 503: Site temporarily unavailable</title><body><h1>Error 503</h1><p>{$errorMessage}</p></body></html>";
     exit();
-} else {
-   echo "<p>Good Work Bro. Connected...</p>";
 }
- 
-$query = 'UPDATE modx_system_settings SET value = "default" WHERE key = "manager_theme"';
-$mysqli->query($query);
-echo "<p>You have reset the manager to the default theme.</p>";
-$mysqli->close();
+
+/* start output buffering */
+ob_start();
+
+/* Create an instance of the modX class */
+$modx= new modX();
+if (!is_object($modx) || !($modx instanceof modX)) {
+    ob_get_level() && @ob_end_flush();
+    $errorMessage = '<a href="setup/">MODX not installed. Install now?</a>';
+    @include(MODX_CORE_PATH . 'error/unavailable.include.php');
+    header('HTTP/1.1 503 Service Unavailable');
+    echo "<html><title>Error 503: Site temporarily unavailable</title><body><h1>Error 503</h1><p>{$errorMessage}</p></body></html>";
+    exit();
+}
+
+$modx->initialize('mgr');
+
+$managerTheme = $modx->getObject('modSystemSetting', array('key' => 'manager_theme'));
+if ($managerTheme) {
+    $managerTheme->set('value', 'default');
+    $managerTheme->save();
+}
+
 ?>
